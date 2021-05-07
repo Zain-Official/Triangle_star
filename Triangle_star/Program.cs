b0c83c66704c5288165c385c73447f431776915e
Zain <zainofficial03@gmail.com>
2021-05-08 01:27:18 +0500

    Add project files.

---
+++ Triangle_star/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Triangle_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i=num; i<=num; i--)
+            {
+               
+                for(int j=1; j<=i; j++)
+                {
+                   
+                    Console.Write("*");
+                }
+
+
+                Console.WriteLine();
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
